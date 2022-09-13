650e4d798510a6c6a0862d5114babb767a1ce234
mujeeb <mujeebasariboss143@gmail.com>
2022-09-13 13:27:06 +0530

    used react-helmetfor titles

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import Helmet from 'react-helmet';
 import Header from '../Header'
 
 const Home = props => {
   return (
     <>
+    <Helmet>
+        <title>
+            Galary
+        </title>
+    </Helmet>
     <Header />
         <div>Home</div>
     </>
--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import Helmet from 'react-helmet'
 import PropTypes from 'prop-types'
 
 const Profile = props => {
   return (
+    <>
+    <Helmet>
+    <title>Profile</title>
+    </Helmet>
     <div>Profile</div>
+    </>
+    
   )
 }
 
